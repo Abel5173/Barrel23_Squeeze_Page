1154718af6687533003d6b6bd5818928ba4aaa2b
Abel <abelzeleke5173@gmail.com>
2023-01-11 13:29:30 +0300

    made some changes

--- barrel_23/src/App.js
+++ barrel_23/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import ProductDescription from './components/ProductDescription';
 
 function App() {
   return (
+    <>
     <NavBar/>
+    <ProductDescription />
+    </>
   );
 }
 
--- barrel_23/src/components/ProductDescription.js
+++ barrel_23/src/components/ProductDescription.js
@@ -1,17 +1,48 @@
-import React from "react";
-import { Box, createTheme } from "@mui/system";
-import j from './images/j.jpg'
+// import React from "react";
+// // import image from "https://unsplash.com/photos/6R1rq5yTl7g"
+// import { Box, createTheme } from "@mui/system";
+
+// export default function ProductDescription(){
+//     return(
+//       <div>
+//         <Box
+//         component="img"
+//         sx={{
+//           height: 233,
+//           width: 350,
+//           maxHeight: { xs: 233, md: 167 },
+//           maxWidth: { xs: 350, md: 250 },
+//         }}
+//         alt="The house from the offer."
+//         src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+//       />
+    
+//     <img src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"/>
+//     </div>
+//     )
+// }
+
+
+
+
+import React from 'react';
+
+import Paper from '@mui/material/Paper';
+import Image from 'src/images/j.jpg'; // Import using relative path
+
 
 const styles = {
     paperContainer: {
-        backgroundImage: `url(${j})`
+        backgroundImage: `url(${Image})`
     }
-}
+};
 
-export default function ProductDescription(){
-    return(
-        <Box style={{
-            backgroundImage: `url(${j})`
-        }}></Box>
-    )
+export default class Home extends React.Component{
+    render(){
+        return(
+            <Paper style={styles.paperContainer}>
+                Some text to fill the Paper Component
+            </Paper>
+        )
+    }
 }
