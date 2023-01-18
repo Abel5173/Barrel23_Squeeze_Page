3295f3a42747c0310e7f912a62413c67b303c17d
Abel <abelzeleke5173@gmail.com>
2023-01-18 10:42:06 +0300

    added collection component

--- barrel_23/src/App.js
+++ barrel_23/src/App.js
@@ -40,6 +40,65 @@ function App() {
             top: "1149.76px",
           }}
         />
+        <br />
+        <br />
+        <img
+          src={image2}
+          alt="Collection"
+          style={{
+            position: "absolute",
+            width: "440px",
+            height: "592px",
+            left: "194px",
+            top: "1669px",
+          }}
+        />
+        <Typography>
+          <Typography
+            variant="h5"
+            sx={{
+              position: "absolute",
+              width: "457px",
+              height: "86px",
+              left: "735px",
+              top: "1736px",
+              fontFamily: "Market Deco",
+              fontStyle: "normal",
+              fontWeight: 400,
+              fontSize: "48px",
+              lineHeight: "90%",
+              /* or 43px */
+              letterSpacing: "-0.06em",
+              color: "#FFFFFF",
+            }}
+          >
+            The Bhakta 50 Collection
+            <br />
+            <br />
+          </Typography>
+          <Box
+            style={{
+              position: "absolute",
+              width: "493px",
+              height: "285px",
+              left: "735px",
+              top: "1854px",
+              fontFamily: "Libre Franklin",
+              fontStyle: "normal",
+              fontWeight: "700",
+              fontSize: "24px",
+              lineHeight: "140%",
+              /* or 28px */
+              letterSpacing: "0.04em",
+              color: "#FFFFFF",
+            }}
+          >
+            J.P. Morgan
+
+Like the innovative banking titan for which it is named, Barrel 23: “Morgan” sets a new “gold standard” for exquisite, investment-grade spirits. Crafted from eight different Armagnac vintages—including 1893, the year J.P. Morgan rescued the U.S. Treasury from collapse—with BHAKTA 50 “Morgan” we honor the titans and tycoons who forged the modern world.
+          </Box>
+          <Box></Box>
+        </Typography>
       </Box>
     </>
   );
