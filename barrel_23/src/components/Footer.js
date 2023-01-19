95208cd4c31c73920e2532959be43d5c4696bc5a
Abel <abelzeleke5173@gmail.com>
2023-01-19 17:09:35 +0300

    added honor component

--- barrel_23/src/components/Footer.js
+++ barrel_23/src/components/Footer.js
@@ -1,51 +1,84 @@
-import { Stack, Grid, Typography, TextField, Button } from "@mui/material";
-import { Box } from "@mui/system";
 import React from "react";
+import { Box } from "@mui/system";
+import { Stack, Typography, Button, TextField } from "@mui/material";
 
-const Footer = () => {
+export default function Footer() {
+  const style = {
+    borderRaduis: "0px",
+    border: "2.8px solid #1E1E1E",
+    fontFamily: "Libre Franklin",
+    fontStyle: "normal",
+    fontWeight: 600,
+    fontSize: "20px",
+    lineHeight: "140%",
+    letterSpacing: "0.04em",
+    textTransform: "uppercase",
+    color: "#1E1E1E",
+  };
   return (
     <Box
-    //   style={{
-    //     position: "absolute",
-    //     // width: 1440px;
-    //     // left: 0px;
-    //     top: "5348px",
-    //     // border-radius: 0px;
-    //   }}
-      mt={2}
-      height={400}
-      sx={{ height: 300 }}
+      sx={{
+        pt: "6em",
+        position: "relative",
+        left: "0px",
+        top: "5370px",
+        backgroundColor: "white",
+        zIndex: 5,
+      }}
     >
-      <Stack
-        direction="row"
-        alignItems={"center"}
-        justifyContent={"space-around"}
-      >
-        <Box>
-          <Typography>STAY UP TO,DATE ON THE WORLD'S OLDEST SPRITS</Typography>
-          <Typography>
-            What's old is newer than ever.Receive <br /> updates on product
-            release,news <br /> events and more.
+      <Stack direction="row" spacing={5} justifyContent="center">
+        <Stack spacing={5}>
+          <Typography
+            style={{
+              // position: absolute;
+              width: "430px",
+              height: "129px",
+              // left: 267px,
+              // top: 5468px,
+              fontFamily: "Market Deco",
+              fontStyle: "normal",
+              fontWeight: 400,
+              fontSize: "48px",
+              lineHeight: "90%",
+              letterSpacing: "-0.06em",
+              color: "#1E1E1E",
+            }}
+          >
+            Stay up to date on the world's oldest spirits
           </Typography>
-        </Box>
-        <Box>
-          <form>
-            <Stack direction={"column"} spacing={1}>
-              <TextField
-                label="Your-full-name"
-                placeholder="name"
-                sx={{ width: 400 }}
-              />
-              <TextField label="your E-mail" placeholder="E-mail" />
-              <Button variant="contained" sx={{ width: 50 }}>
-                Submit
-              </Button>
-            </Stack>
-          </form>
-        </Box>
+          <Typography
+            style={{
+              width: "430px",
+              height: "129px",
+              fontFamily: "Libre Franklin",
+              fontStyle: "normal",
+              fontWeight: 400,
+              fontSize: "20px",
+              lineHeight: "150%",
+              letterSpacing: "0.04em",
+              color: "#1E1E1E",
+            }}
+          >
+            What’s old is newer than ever. Receive updates on product releases,
+            news, events and more.
+          </Typography>
+        </Stack>
+
+        <Stack direction="column" spacing={3}>
+          <TextField sx={{
+            width: "100%"
+          }}
+          style={style} label="Your full name" variant="outlined" />
+          <TextField style={style} label="You email" variant="outlined" />
+          <Button sx={{
+            width:170,
+            borderRadius: 0
+          }}
+          style={style} variant="outlined">
+            Sign up
+          </Button>
+        </Stack>
       </Stack>
     </Box>
   );
-};
-
-export default Footer;
+}
